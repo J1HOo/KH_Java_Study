996f029a1bcdffd758f3a8a6d6a3aaaa1c92d58d
박지호 <businessj1ho@gmail.com>
2024-11-18 17:52:19 +0900

    java Scanner

--- 01_Varible/src/com/variable/practice/Practice1.java
+++ 01_Varible/src/com/variable/practice/Practice1.java
@@ -4,5 +4,12 @@ import java.util.Scanner;
 
 public class Practice1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("문자를 입력하세요.");
+        char text = sc.next().charAt(0);
+        int changeNum = (int)text;
+        System.out.println("1번 text 변수 값 확인하기 :" + text);
+        System.out.println("2번 changeNum 변수 값 확인하기 :" + changeNum);
     }
 }
