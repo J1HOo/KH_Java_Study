c86549d45b47df84ed6aa2112f42251e4fb92b04
박지호 <businessj1ho@gmail.com>
2024-11-18 18:10:47 +0900

    java Scanner

--- 01_Varible/src/com/variable/practice/Practice3.java
+++ 01_Varible/src/com/variable/practice/Practice3.java
@@ -17,6 +17,12 @@ public class Practice3 {
         System.out.println(iNum2 * dNum); // 10.0
         System.out.println((double) iNum1); // 10.0
 
+        System.out.println((double) iNum1 / iNum2); // 2.5
+        System.out.println(dNum); // 2.5
+
+        System.out.println((int) fNum); // 3
+        System.out.println(iNum1 / (int) fNum); // 3
+
         System.out.println(iNum1 / fNum); // 3.3333333
         System.out.println((double) iNum1 / fNum); // 3.3333333335
 
