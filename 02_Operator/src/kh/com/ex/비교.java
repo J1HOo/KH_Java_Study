0b095d93bcd554b03a62b6113ff389eac50d62f5
박지호 <businessj1ho@gmail.com>
2024-11-19 17:49:50 +0900

    Java equals

--- 02_Operator/src/kh/com/ex/비교.java
+++ 02_Operator/src/kh/com/ex/비교.java
@@ -15,11 +15,24 @@ public class 비교 {
     *
     *   값(value) : 문자열의 실제 내용
     *
-    *   메모리 주소(Memory Address) : 객체의 위치, 우리가 값을 변수에 저장하면 컴퓨터는 그 값을 메모리에 저장 후 주소를 기억
+    *    메모리 주소(Memory Address) : 객체의 위치, 우리가 값을 변수에 저장하면 컴퓨터는 그 값을 메모리에 저장 후 주소를 기억
     *
     *   == 과 .equals() 의 차이
-    *   ==는 메모리 주소를 비교, 두 변수가 가르키고 있는 메모리 주소가 같으면 true
-    *   .equals()는 두 변수의 실제 값(value)이 같다면 true
+    *    ==는 메모리 주소를 비교, 두 변수가 가르키고 있는 메모리 주소가 같으면 true
+    *    .equals()는 두 변수의 실제 값(value)이 같다면 true
     *
+    *   equalsIgnoreCase()
+    *     대소문자를 구분하지 않고 문자열을 비교하는 메서드
+    *     ex)
+    *     String a = "Hello"
+    *     String b = "HELLO"
+    *
+    *     a.(equalsIgnoreCase(b) // true
+    *     b.(equalsIgnoreCase("hi") // false
+    *
+    *    .equalIgnoreCase() : 문자열 내용이 같으면 대소문자는 구분 X
+    *                        -> 응답을 받을 때 대소문자를 거르지 않기 위해
+    *    .equals() : 문자열이 대소문자도 차이나면 안됨
+
     * */
 }
