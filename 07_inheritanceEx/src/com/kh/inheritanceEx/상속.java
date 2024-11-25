339c4df78cae0ae6c79526f1627fb305d700298f
박지호 <businessj1ho@gmail.com>
2024-11-25 16:03:05 +0900

    Java 상속

---
+++ 07_inheritanceEx/src/com/kh/inheritanceEx/상속.java
@@ -0,0 +1,9 @@
+package com.kh.inheritanceEx;
+
+// 부모의 코드를 물려 받아 자식이 자신의 것처럼 사용
+// 상속 키워드 : extends (확장하다) -> 부모 코드를 자식이 물려 받아 자식의 크기가 확장됨
+
+// Object : 모든 클래스의 접근 가능한 최상위 부모 클래스
+// 상속을 선언하지 않아도 자동으로 extends Object가 숨김처리 된 상태
+public class 상속 extends Object {
+}
