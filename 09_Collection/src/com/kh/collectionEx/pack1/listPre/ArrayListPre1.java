15212d0059106283686c34ae2cb6391e0db94d7e
박지호 <businessj1ho@gmail.com>
2024-11-26 12:24:08 +0900

    Java arrayList

--- 09_Collection/src/com/kh/collectionEx/pack1/listPre/ArrayListPre1.java
+++ 09_Collection/src/com/kh/collectionEx/pack1/listPre/ArrayListPre1.java
@@ -45,7 +45,36 @@ public class ArrayListPre1 {
     }
 
     public void method2() {
+        // ArrayList를 생성하여사과, 포도, 바나나, 망고를 저장하세요.
         ArrayList<String> list = new ArrayList<>();
+        list.add("사과");
+        list.add("포도");
+        list.add("망고");
+        list.add("바나나");
+
+        // 과일 리스트의 두 번째 위치에 오렌지를 추가하세요.
+        list.add(1, "오렌지");
+
+        // 리스트에서 바나나 -> 파인애플로 변경하세요.
+        list.set(3, "파인애플");
+
+        // 리스트에서 첫 번째 과일을 삭제하세요.
+        list.remove(0);
+
+        // 리스트에서 망고를 삭제하세요.
+        list.remove("망고");
+
+        // 리스트에 사과가 남아있는지 확인하고 사과가 만약에 있다면 사과를 없애고 출력하세요.
+        if (list.contains("사과") == true) {
+            list.remove("사과");
+            System.out.println(list);
+        } else {
+            System.out.println("사과가 리스트에 없습니다.");
+        }
+
+        // 리스트를 비우고, 리스트가 비어 있는지 확인하고 결과를 출력 하세요.
+        list.clear();
+        System.out.println("과일 바구니가 비었는가 " + list.isEmpty());
 
     }
 }
--- 09_Collection/src/com/kh/collectionEx/pack1/listPre/ArrayListRun1.java
+++ 09_Collection/src/com/kh/collectionEx/pack1/listPre/ArrayListRun1.java
@@ -3,6 +3,7 @@ package com.kh.collectionEx.pack1.listPre;
 public class ArrayListRun1 {
     public static void main(String[] args) {
         ArrayListPre1 list = new ArrayListPre1();
-        list.method1();
+        // list.method1();
+        list.method2();
     }
 }
