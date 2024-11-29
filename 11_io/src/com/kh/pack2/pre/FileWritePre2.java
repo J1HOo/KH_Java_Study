73f3d0565a355d8949201eae7e4f592017df3e56
박지호 <businessj1ho@gmail.com>
2024-11-29 12:20:48 +0900

    Java file, write

---
+++ 11_io/src/com/kh/pack2/pre/FileWritePre2.java
@@ -0,0 +1,36 @@
+package com.kh.pack2.pre;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+
+public class FileWritePre2 {
+    public boolean method1() {
+        File file = new File(System.getProperty("user.home") + "/Desktop/lunch.txt");
+
+        if (file.exists()) {
+            System.out.println("이미 존재하는 파일입니다.");
+            return false;
+        }
+
+        try (Writer writer = new FileWriter(file)) {
+            if (file.exists()) {
+                file.createNewFile();
+                writer.write("제육볶음 \n치킨 \n저녁으로 닭발");
+                System.out.println("파일 작성 완료");
+                }
+            } catch (Exception e) {
+            System.out.println("파일 만들기에 실패: " + e);
+            return false;
+
+        } finally {
+            System.out.println("프로그램 종료");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        FileWritePre2 fwp = new FileWritePre2();
+        fwp.method1();
+    }
+}
